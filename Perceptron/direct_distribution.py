e7736c80fa9428404830e66b0966b7841187221a
Roman <roma.bashirov.1@yandex.ru>
2023-07-15 14:00:11 +0300

    Закончил поэтапную роспись прямого распространения

--- Perceptron/direct_distribution.py
+++ Perceptron/direct_distribution.py
@@ -117,7 +117,7 @@
 # print(pred)
 
 # ------------------------ Несколько входов и выходов ------------------------ #
-#         игр % побед # болельщиков
+#         игр % побед болельщиков
 # weights = [[0.1, 0.1, -0.3],                        # травмы?
 #             [0.1, 0.2, 0.0],                        # победа?
 #             [0.0, 1.3, 0.1]]                        # печаль?
@@ -151,4 +151,69 @@
 # pred = neural_network(input, weights)
 # print(pred)
 
-# ------------------------ Несколько входов и выходов ------------------------ #
+# ------------------------ Прогнозирование на основе прогнозов ------------------------ #
+# #       игр % побед болельщиков
+# ih_wgt = [[0.1, 0.2, -0.1],                         # hid[0]
+#           [-0.1,0.1, 0.9],                          # hid[1]
+#           [0.1, 0.4, 0.1]]                          # hid[2]
+# #       hid[0] hid[1] hid[2]
+# hp_wgt = [[0.3, 1.1, -0.3],                         # травмы?
+#           [0.1, 0.2, 0.0],                          # победа?
+#           [0.0, 1.3, 0.1]]                          # печаль?
+# weights = [ih_wgt, hp_wgt]
+#
+#
+# def w_sum(vector, weights):                         # векторная сумма
+#     assert(len(vector) == len(weights))
+#     output = 0
+#     for i in range(len(vector)):
+#         output += (vector[i] * weights[i])
+#     return output
+#
+#
+# def vect_mat_mul(vector, matrix):                   # типо матричное умножение
+#     assert(len(vector) == len(matrix))
+#     output = [0] * len(matrix)
+#     for i in range(len(vector)):
+#         output[i] = w_sum(vector, matrix[i])
+#     return output
+#
+#
+# def neural_network(input, weights):                 # НС, ничего нового
+#     hid = vect_mat_mul(input, weights[0])
+#     pred = vect_mat_mul(hid, weights[1])
+#     return pred
+#
+#
+# toes = [8.5, 9.5, 9.9, 9.0]                         # текущее среднее число игр
+# wlrec = [0.65, 0.8, 0.8, 0.9]                       # процент побед
+# nfans = [1.2, 1.3, 0.5, 1.0]                        # число болельщиков
+# input = [toes[0],wlrec[0],nfans[0]]
+# pred = neural_network(input, weights)
+# print(pred)
+
+# ------------------------ Ура, снова NumPy ------------------------ #
+# import numpy as np
+# #       игр % побед болельщиков
+# ih_wgt = np.array([[0.1, 0.2, -0.1],                # hid[0]
+#                    [-0.1,0.1, 0.9],                 # hid[1]
+#                    [0.1, 0.4, 0.1]]).T              # hid[2]
+# #       hid[0] hid[1] hid[2]
+# hp_wgt = np.array([[0.3, 1.1, -0.3],                # травмы?
+#                    [0.1, 0.2, 0.0],                 # победа?
+#                    [0.0, 1.3, 0.1]]).T              # печаль?
+# weights = [ih_wgt, hp_wgt]
+#
+#
+# def neural_network(input, weights):
+#     hid = input.dot(weights[0])
+#     pred = hid.dot(weights[1])
+#     return pred
+#
+#
+# toes = np.array([8.5, 9.5, 9.9, 9.0])
+# wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+# nfans = np.array([1.2, 1.3, 0.5, 1.0])
+# input = np.array([toes[0],wlrec[0],nfans[0]])
+# pred = neural_network(input, weights)
+# print(pred)
