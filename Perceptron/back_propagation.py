0f0d4c40468abd4e07c9f6b3eae5e307ec395a9e
Roman <roma.bashirov.1@yandex.ru>
2023-07-17 16:27:31 +0300

    Поэтапное изучение обратного распространения v.2

--- Perceptron/back_propagation.py
+++ Perceptron/back_propagation.py
@@ -66,40 +66,46 @@
 # print(layer_2)
 
 # ------------------------ Обратное распространение в коде ------------------------ #
-# import numpy as np                                                              # импорт, что логично
-# np.random.seed(1)                                                               # установка сида,
-# binary = np.array([[0,0,1],                                                     # чтобы менять гиперпараметры
-#                    [0,1,0],                                                     # и видеть разницу, что невозможно с
-#                    [1,1,0],                                                     # разными начальными весами
-#                    [1,0,1]])                                                    #
-# goal =np.array([1,0,0,1]).T                                                     #
-# alpha = .1                                                                      #
-# hidden_size = 4                                                                 # количество узлов в слое
-# weights01 = np.random.random((3, hidden_size))                                  # установка начальных весов
-# weights12 = np.random.random((hidden_size, 1))                                  # установка начальных весов
-#
-#
-# def relu(x):                                                                    # функция relu, чтобы отключить
-#     return (x > 0) * x                                                          # отрицательные веса
-#
-#
-# def relu2rediv(x):                                                              # функция, чтобы не менять параметры
-#     return x > 0                                                                # выключенных весов
-#
-#
-# for iter in range(60):                                                          # 60 эпох обучения
-#     error_all = 0                                                               # сумма всех ошибок
-#     for i in range(len(binary)):                                                # проход по каждому входному набору
-#         layer_0 = binary[i:i+1]                                                 # входной набор
-#         layer_1 = relu(np.dot(layer_0, weights01))                              # параметры скрытого слоя
-#         layer_2 = layer_1.dot(weights12)                                        # результат предсказания
-#         error_all = np.sum((goal[i:i+1] - layer_2) ** 2)                        # сумма квадратичных ошибок
-#         error_2_delta = goal[i:i+1] - layer_2                                   # отклонение от истинного результата
-#         error_1_delta = error_2_delta.dot(weights12.T) * relu2rediv(layer_1)    # расчет отклонения для скрытого слоя
-#         weights12 += alpha * (layer_1.T.dot(error_2_delta))                     # изменение весов
-#         weights01 += alpha * (layer_0.T.dot(error_1_delta))                     # изменение весов
-#         print(f'Layer{i}: {layer_2[0][0]:.2f}')                                 # вывод результата по входным параметрам
-#     print(f"Error: {error_all:.5f}\n")                                          # вывод суммы ошибок
+import numpy as np                                                              # импорт, что логично
+np.random.seed(1)                                                               # установка сида,
+binary = np.array([[0,0,1],                                                     # чтобы менять гиперпараметры
+                   [0,1,0],                                                     # и видеть разницу, что невозможно с
+                   [1,1,0],                                                     # разными начальными весами
+                   [1,0,1]])                                                    #
+goal =np.array([1,0,0,1]).T                                                     #
+alpha = .1                                                                      #
+hidden_size = 4                                                                 # количество узлов (нейронов) в слое
+weights01 = np.random.random((3, hidden_size))                                  # установка начальных весов
+weights12 = np.random.random((hidden_size, 1))                                  # установка начальных весов
+
+
+def relu(x):                                                                    # функция relu, чтобы отключить
+    return (x > 0) * x                                                          # отрицательные веса
+
+
+def relu2rediv(x):                                                              # функция, чтобы не менять параметры
+    return x > 0                                                                # выключенных весов
+
+
+epochs = 0                                                                      # количество необходимых эпох для обучения
+while True:                                                                     # запуск обучения
+    epochs += 1                                                                 # увеличение количества эпох на 1
+    error_all = 0                                                               # сумма всех ошибок
+    for i in range(len(binary)):                                                # проход по каждому входному набору
+        layer_0 = binary[i:i+1]                                                 # входной набор
+        layer_1 = relu(np.dot(layer_0, weights01))                              # параметры скрытого слоя
+        layer_2 = layer_1.dot(weights12)                                        # результат предсказания
+        error_all = np.sum((goal[i:i+1] - layer_2) ** 2)                        # сумма квадратичных ошибок
+        error_2_delta = goal[i:i+1] - layer_2                                   # отклонение от истинного результата
+        error_1_delta = error_2_delta.dot(weights12.T) * relu2rediv(layer_1)    # расчет отклонения для скрытого слоя
+        weights12 += alpha * (layer_1.T.dot(error_2_delta))                     # изменение весов
+        weights01 += alpha * (layer_0.T.dot(error_1_delta))                     # изменение весов
+        print(f'Layer{i}: {layer_2[0][0]:.9f}')                                 # вывод результата по входным параметрам
+    print(f"Error: {error_all:.10f}\n")                                         # вывод суммы ошибок
+    if round(error_all, 9) < 10e-10:                                            # остановка обучения, если ошибка
+        break                                                                   # меньше целевого значения
+print(f"Epochs: {epochs}")                                                      # вывод результата необходимых эпох
+
 
 
 
